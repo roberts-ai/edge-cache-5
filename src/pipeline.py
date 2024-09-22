a1d0158b36fd37bd3df6b502fc7734fbace8b134
robert <montey@meppelink.net>
2024-09-22 10:29:07 +0000

    Adding cache

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,34 +1,40 @@
 import torch
 from PIL.Image import Image
+from diffusers import StableDiffusionXLPipeline,DPMSolverMultistepScheduler
 from pipelines.models import TextToImageRequest
 from torch import Generator
-from diffusers import StableDiffusionXLPipeline
+from tgate import TgateSDXLDeepCacheLoader
 
+GATE_STEP = 10
+INF_STEP= 25
 
 def load_pipeline() -> StableDiffusionXLPipeline:
     pipeline = StableDiffusionXLPipeline.from_pretrained(
-        "./models/newdream-sdxl-21",
-        torch_dtype=torch.float16,
+        "./models/newdream-sdxl-21/",
+        torch_dtype=torch.float16, 
+        use_safetensors=True, 
         local_files_only=True,
-    ).to("cuda", torch.float16)
-    #pipeline(prompt="")
+    )
 
-    # Warmup
-    with torch.inference_mode():
-        pipeline("")
+    pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
 
-
+    pipeline = TgateSDXLDeepCacheLoader(
+                   pipeline,
+                          cache_interval=3,
+                                 cache_branch_id=0,
+                                 ).to("cuda")
+    pipeline(prompt="")
     return pipeline
-
 
 def infer(request: TextToImageRequest, pipeline: StableDiffusionXLPipeline) -> Image:
     generator = Generator(pipeline.device).manual_seed(request.seed) if request.seed else None
-
-    return pipeline(
+    return pipeline.tgate(
         prompt=request.prompt,
-        negative_prompt=request.negative_prompt,
+        negative_prompt=f"{request.negative_prompt}",
         width=request.width,
         height=request.height,
-        num_inference_steps=30,
         generator=generator,
+        num_inference_steps=INF_STEP,
+        gate_step=GATE_STEP,
     ).images[0]
+
